9933887a3d1c41c90d4b29de3b89b38486b4a265
OrangeScript <da.naranjo10@uniandes.edu.co>
2025-01-16 21:44:40 -0500

    fix(footer): fix footer to only show social media logos on mobile view

--- src/MyFooter/index.js
+++ src/MyFooter/index.js
@@ -7,10 +7,10 @@ function MyFooter () {
         <footer class="myFooter">
             <Container class="content">
                 <Row>
-                    <Col>
+                    <Col class='col-1'>
                         <a><button class="contactme_button" role="button">Contact Me!</button></a>
                     </Col>
-                    <Col>
+                    <Col class='col-2'>
                         <a class="icon" target="_blank" href="https://www.instagram.com/imorange.dev/"><img class="icon" src="https://upload.wikimedia.org/wikipedia/commons/e/e7/Instagram_logo_2016.svg"></img></a>
                         <a class="icon" target="_blank" href="https://x.com/imorangedev"><img class="icon" src="https://upload.wikimedia.org/wikipedia/commons/c/ce/X_logo_2023.svg"></img></a>
                         <a class="icon" target="_blank" href="https://www.linkedin.com/in/imorangedev/"><img class="icon" src="https://upload.wikimedia.org/wikipedia/commons/8/81/LinkedIn_icon.svg"></img></a>
