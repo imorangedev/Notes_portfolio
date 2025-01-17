a78d15a5f7f3f174647e846450b62f2b54e33b2f
OrangeScript <da.naranjo10@uniandes.edu.co>
2025-01-16 20:43:39 -0500

    improve(header): improve header to be mobile first

--- src/MyHeader/index.js
+++ src/MyHeader/index.js
@@ -7,8 +7,9 @@ function MyHeader () {
             <a class="logo-link" href='https://github.com/imorangedev' target="_blank">
                 <span><img class="main-logo" src="https://portfoliodiegodev.s3.us-east-1.amazonaws.com/assets/img/logo.png"></img></span>
             </a>
-            <a class='nav-links' href='https://drive.google.com/file/d/1zgyzYY6NTQKaAMUSzsH7MhCS1MMKyo9h/view?usp=sharing' target="_blank">CV</a>
-            
+            <nav class="nav-links">
+                <a class='nav-links' href='https://drive.google.com/file/d/1zgyzYY6NTQKaAMUSzsH7MhCS1MMKyo9h/view?usp=sharing' target="_blank">CV</a>
+            </nav>
         </header>
     );
 }
